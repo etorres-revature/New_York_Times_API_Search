17c6251301eb02b49fceb4f79ba6abc4a6c1b09b
Eric D. Torres <etorresnotary@gmail.com>
2020-08-23 00:48:05 -0500

    final working js code ... for now

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var articleCounter = 0;
 $("#search").on("click", function () {
     event.preventDefault();
 
+    $(".articles").empty();
+
     searchTerms = $("#inputSearchTerms").val().trim();
     // console.log(searchTerms);
     let searchURL = queryURLBase + "&q=" + searchTerms;
@@ -19,7 +21,7 @@ $("#search").on("click", function () {
 
     if (parseInt(beginDate)) {
         searchURL = queryURLBase + "&begin_date=" + beginDate + "0101";
-        // console.log(searchURL);
+        console.log(searchURL);
     }
 
     if (parseInt(endDate)) {
@@ -28,7 +30,7 @@ $("#search").on("click", function () {
     }
 
     numResults = $("#inputNumber").val();
-    // console.log(numResults);
+    console.log(numResults);
 
     $.ajax({
         url: searchURL,
@@ -39,13 +41,12 @@ $("#search").on("click", function () {
             // console.log(dataFromNYT.response.docs[0].abstract);
             // console.log(dataFromNYT.response.docs[0].web_url);
             // console.log(dataFromNYT.response.docs[0].headline.main);
-
             // let results = response.docs;
             // console.log(results);
 
             for (var i = 1; i <= numResults; i++) {
                 // console.log("in the for loop")
-                var articleUL = $("<div>").addClass("card-body").attr("id", "card-body-" + i).css("list-style-type", "none");
+                var articleDiv = $("<div>").addClass("card-body").attr("id", "card-body-" + i).css("list-style-type", "none");
 
                 var headline = dataFromNYT.response.docs[i].headline.main;
                 var abstract = dataFromNYT.response.docs[i].abstract;
@@ -54,14 +55,26 @@ $("#search").on("click", function () {
                 var aURL = $("<a>").attr("href", webURL).text(webURL)
                 var pAbstract = $("<p>").text(abstract);
 
+                // if (headline === null) {
+                //     pheadline = $("<hr>").text("No headline available");
+                // }
 
-                articleUL.append(pHeadline);
-                articleUL.append(aURL);
-                articleUL.append(pAbstract);
-                articleUL.append("<hr>");
+                articleDiv.append(pHeadline);
+                articleDiv.append(aURL);
+                articleDiv.append(pAbstract);
+                articleDiv.append("<hr>");
 
-                $(".list-group-flush").append(articleUL);
+                $(".articles").append(articleDiv);
             }
-        });
 
+            $("#inputSearchTerms").val("");
+            $("#inputStartYear").val("");
+            $("#inputEndYear").val("");
+
+        });
 });
+
+$("#clear").on("click", function(){
+event.preventDefault();
+$(".articles").empty();
+})
